6060844970aaada819b13e1ba4d9c5206a37de2d
Nandana0987 <nandanahari098@gmail.com>
2025-10-17 20:22:30 +0530

    Add files via upload

---
+++ github1.py
@@ -0,0 +1,10 @@
+num1=int(input("enter first number:"))
+num2=int(input("enter second number:"))
+sum=num1+num2
+diff=num1-num2
+prod=num1*num2
+div=num1/num2
+print(f"Addition:{sum}")
+print(f"Subtraction:{diff}")
+print(f"Multiplication:{prod}")
+print(f"Division: {div}")
